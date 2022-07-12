5765aa296e122986fb1271ecb027eec20bebae55
otabekf13 <otabek.f13@gmail.com>
2022-07-13 00:43:03 +0500

    some fixes

--- script.js
+++ script.js
@@ -1,39 +1,58 @@
+let stname = document.querySelector('stname').value
+let age = document.querySelector('age').value
 let btn = document.querySelector('button')
 let form = document.forms.names
 let delStudent = document.getElementById('delStudent')
+let content = document.querySelector('.content')
+
+// Модальное окно
+let modal = document.getElementById("modal");
+let editBtn = document.querySelectorAll("edit");
+let closeBtn = document.getElementsByClassName("close")[0];
+
+editBtn.onclick = function () {
+	modal.style.display = "block";
+}
+closeBtn.onclick = function () {
+	modal.style.display = "none";
+}
+window.onclick = function (event) {
+	if (event.target == modal) {
+		modal.style.display = "none";
+	}
+}
 
 //
 let students = [
 	{
-		id: 1,
-		name: 'wwssaa sddff',
-		age: 21
+		id: '',
+		name: '',
+		age: ''
 	},
 	{
-		id: 2,
-		name: 'basfd dds23',
-		age: 24
+		id: '',
+		name: '',
+		age: ''
 	}
 ]
-
+// нумерация
 const nums = [...document.querySelectorAll('.id')]
 nums.forEach((num, index) => num.innerHTML = ++index)
 
+//
 form.onsubmit = (event) => {
 	event.preventDefault()
 
 	let student = {
-		id: nums,
-		name: 'ogoo gle',
-		age: 21
+		id = nums,
+		name =  stname,
+		age = birth
 	}
 
 	students.push(student)
 
 	reload(students)
-
 }
-
 
 
 function reload(arr) {
@@ -42,14 +61,14 @@ function reload(arr) {
 	for (let item of arr) {
 		let num = document.createElement('span')
 		let student = document.createElement('span')
-		let birht = document.createElement('span')
+		let birth = document.createElement('span')
 		let actions = document.createElement('div')
 		let edit = document.createElement('span')
 		let delStudent = document.createElement('span')
 
 		num.innerHTML = item.id
 		student.innerHTML = item.name
-		birht.innerHTML = item.age
+		birth.innerHTML = item.age
 		actions.classList.add('actions')
 		edit.innerHTML = `<i class="fa - regular fa - pen - to - square"></i>`
 		edit.setAttribute('id', 'edit')
@@ -66,21 +85,6 @@ function reload(arr) {
 		}
 	}
 }
-// Модальное окно
-let modal = document.getElementById("modal");
-let editBtn = document.getElementById("edit");
-let closeBtn = document.getElementsByClassName("close")[0];
 
-editBtn.onclick = function () {
-	modal.style.display = "block";
-}
-closeBtn.onclick = function () {
-	modal.style.display = "none";
-}
-window.onclick = function (event) {
-	if (event.target == modal) {
-		modal.style.display = "none";
-	}
-}
 
 reload(students)
